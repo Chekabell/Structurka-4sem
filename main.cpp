d245cf09c9d4d2df5cf5eed8b6c26f38261d8e56
Chekabell <nikita.toporischev@mail.ru>
2024-04-25 20:45:34 +0500

    Intermediate commit

--- main.cpp
+++ main.cpp
@@ -1,78 +1,162 @@
 ﻿#include <iostream>
 
+int globalCount = 0;
+
 int* genArray(const int len) {
 	int* array = new int[len];
-	for (int i = 0; i < len; i++) array[i] = i;
+	for (int i = 1; i < len; i++) array[i] = i;
 	return array;
 }
 
-int searchSequential(int* array, const int len, const int searchingNumber, const int begin = 0) {
-	if (array && len > 0) {
-		for (int i = begin; i < len; i++) {
-			if (array[i] == searchingNumber) {
-				return array[i];
-			}
+int searchSequential(int* array, const int begin, const int end, const int searchingNumber) {
+	for (int i = begin; i <= end; i++) {
+		if (globalCount++, array[i] == searchingNumber) {
+			return array[i];
 		}
 	}
 	return 0;
 }
 
 int searchJumpLVL1(int* array, const int len, const int searchingNumber) {
-	int i = sqrt(len);
-	while (array[i] < searchingNumber)
-		i += sqrt(len);
-	return searchSequential(array, i, searchingNumber);
+	int jump = sqrt(len);
+	int stepLeft = 0;
+	int stepRight = jump;
+	while (globalCount++, array[stepRight] < searchingNumber) {
+		stepLeft = stepRight;
+		stepRight += jump;
+		if (stepRight > len) return 0;
+	}
+	return searchSequential(array, stepLeft, stepRight, searchingNumber);
 }
 
 int searchJumpLVL2(int* array, const int len, const int searchingNumber) {
-	int i = sqrt(len);
-	while (array[i] < searchingNumber)
-		i += sqrt(len);
-	while (array[i] > searchingNumber)
-		i -= sqrt(len);
-	return searchSequential(array, len, searchingNumber, i);
+	int jump = sqrt(len);
+	int stepLeft = 0;
+	int stepRight = jump;
+	int fixStepRight;
+	while (globalCount++, array[stepRight] < searchingNumber) {
+		stepLeft = stepRight;
+		stepRight += jump;
+		if (stepRight > len) return 0;
+	}
+	fixStepRight = stepRight;
+	while (globalCount++, array[stepLeft] > searchingNumber) {
+		stepRight = stepLeft;
+		stepLeft -= jump;
+		if (stepLeft < 0) return 0;
+	}
+	return searchSequential(array, stepLeft, fixStepRight, searchingNumber);
 }
 
-int searchBinary(int* array,const int begin, const int end, const int searchingNumber) {
-	int mid = end + begin / 2;
-	if (array[mid] < searchingNumber)
-		return searchBinary(array, mid+1, end, searchingNumber);
-	else if (array[mid] > searchingNumber)
-		return searchBinary(array, begin, mid-1,searchingNumber);
-	return array[mid];
+int searchBinary(int* array, int begin, int end, const int searchingNumber) {
+	int mid;
+	while (begin <= end) {
+		mid = begin + (end - begin) / 2;
+		if (globalCount++, array[mid] == searchingNumber) return array[mid];
+		else if (globalCount++, array[mid] > searchingNumber) end = mid - 1;
+		else begin = mid + 1;
+	}
+	return 0;
 }
 
 int searchBinary(int* array, const int len, const int searchingNumber) {
 	return searchBinary(array, 0, len-1, searchingNumber);
 }
 
-int searchInterpolation(int* array, const int left, const int right, const int searchingNumber) {
-	int index = (searchingNumber - array[left]) * (left - right) / (array[left] - array[right]) + left;
-	if (array[index] < searchingNumber)
-		return searchBinary(array, index+1, right, searchingNumber);
-	else if (array[index] > searchingNumber)
-		return searchBinary(array, left, index-1, searchingNumber);
-	return array[index];
+int searchInterpolation(int* array, int left, int right, const int searchingNumber) {
+	int index;
+	while (left <= right && (globalCount++, searchingNumber >= array[left]) && (globalCount++, searchingNumber <= array[right])) {
+		index = left + (((right - left) / (array[right] - array[left])) * (searchingNumber - array[left]));
+		if (globalCount++, array[index] == searchingNumber) return array[index];
+		if (globalCount++, array[index] > searchingNumber) right = index - 1;
+		else left = index + 1;
+	}
+	return 0;
 }
 
 int searchInterpolation(int* array, const int len, const int searchingNumber) {
 	return searchInterpolation(array, 0, len - 1, searchingNumber);
 }
 
-void checkSearchFunction(int funcSearch(int*,const int,const int)) {
-	const int len = 1000;
-	int searchingNumber = 254;
-	int* array = genArray(len);
-	if (funcSearch(array, len, searchingNumber) == searchingNumber) {
-		std::cout << "Search is work!" << std::endl;
+void checkSearchFunction(int funcSearch(int*, const int, const int), const int step) {
+	int maxLen = step * 10;
+	for (int nowLen = step; nowLen <= maxLen; nowLen += step) {
+		int j = 0;
+		int searchingNumberExist = rand() % nowLen;
+		int searchingNumberNotExist = -1;
+		globalCount = 0;
+		j = 0;
+		int* array = genArray(nowLen);
+		std::cout << "LEN ARRAY: " << nowLen << std::endl;
+		while (j < 10) {
+			if (funcSearch(array, nowLen, (searchingNumberExist*(j+1))) == searchingNumberExist * (j + 1)) j++;
+			else {
+				std::cout << "FAIL" << std::endl;
+				return;
+			}
+		}
+		std::cout << "Existing  - " << globalCount/j << std::endl;
+		globalCount = 0;
+		if (funcSearch(array, nowLen, searchingNumberNotExist)){
+			std::cout << "FAIL" << std::endl;
+			return;
+		}
+		std::cout << "NOT existing  - " << globalCount << std::endl;
 	}
-	else std::cout << "FAILED" << std::endl;
+}
+
+int searchJumpForResearch(int *array, const int len, const int searchingNumber, const int jump) {
+	int stepLeft = 0;
+	int stepRight = jump;
+	while (globalCount++, array[stepRight] < searchingNumber) {
+		stepLeft = stepRight;
+		stepRight += jump;
+		if (stepRight > len) return 0;
+	}
+	return searchSequential(array, stepLeft, stepRight, searchingNumber);
+}
+
+void researchJump(const int step) {
+	int maxLen = step * 5;
+	int stepJump;
+	for (int nowLen = step; nowLen <= maxLen; nowLen += step) {
+		int searchingNumber = rand() % nowLen;
+		int* array = genArray(nowLen);
+		globalCount = 0;
+		stepJump = 1;
+		std::cout << "LEN ARRAY:" << nowLen << std::endl;
+		while (stepJump <= nowLen / 2) {
+			if (searchJumpForResearch(array, nowLen, searchingNumber, stepJump) == searchingNumber) {
+				std::cout << /*stepJump << ": "<<*//*"Step Jump:: "<< stepJump << " Comparisons: " <<*/ globalCount << std::endl;
+				stepJump++;
+			}
+			else {
+				std::cout << "FAIL" << std::endl;
+				return;
+			}
+		}
+	}
 }
 
 int main(void) {
-	checkSearchFunction(searchBinary);
-	checkSearchFunction(searchJumpLVL1);
-	checkSearchFunction(searchJumpLVL2);
-	checkSearchFunction(searchInterpolation);
+	srand(time(NULL));
+	int step = 100;
+	/*std::cout << "=======BINARY=======" << std::endl;
+	checkSearchFunction(searchBinary,step);
+	std::cout << "====================" << std::endl;
+	std::cout << "====INTERPOLATION====" << std::endl;
+	checkSearchFunction(searchInterpolation, step);
+	std::cout << "=====================" << std::endl;
+	std::cout << "======JUMPLVL1======" << std::endl;
+	checkSearchFunction(searchJumpLVL1, step);
+	std::cout << "====================" << std::endl;
+	std::cout << "======JUMPLVL2======" << std::endl;
+	checkSearchFunction(searchJumpLVL2, step);
+	std::cout << "====================" << std::endl;*/
+	researchJump(step);
+	/*int* array = genArray(1000);
+	std::cout << array[5] << std::endl;
+	std::cout << searchBinary(array, 1000, 5) << std::endl;*/
+
 	return 0;
 }
