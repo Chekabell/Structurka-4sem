7aaa1ebe4285cd16445d8ff1c985543702218122
Chekabell <nikita.toporischev@mail.ru>
2024-05-01 18:12:51 +0500

    Almost done

--- main.cpp
+++ main.cpp
@@ -1,203 +1,109 @@
 ﻿#include <iostream>
+#include <chrono>
 
 int globalCount = 0;
+const char* globalAlphabet{ "QWERTYUIOPASDFGHJKLZXCVBNM"};
 
-int* genArray(const int len) {
-	int* array = new int[len];
-	int number = 1;
+
+char* genText(const int len) {
+	size_t lenAlphabet = strlen(globalAlphabet);
+	char* text = new char[len];
 	for (int i = 0; i < len; i++) {
-		array[i] = number;
-		number += 2;
+		text[i] = globalAlphabet[rand() % lenAlphabet];
 	}
-	return array;
+	return text;
 }
 
-int searchJumpLVL1(int* array, const int len, const int searchingNumber) {
-	const int jump = sqrt(len);
-	int stepLeft = 0;
-	int stepRight = jump;
-	while (globalCount++, array[std::min(stepRight, len) - 1] < searchingNumber) {
-		stepLeft = stepRight;
-		stepRight += jump;
-		if (stepLeft >= len) return 0;
+int* prefixGen(const char* text) {
+	size_t len = strlen(text);
+	int* pi = new int[len]{0};
+	for (int i = 1; i < len; i++) {
+		int j = pi[i - 1];
+		for (; j > 0 && text[j] != text[i];)
+			j = pi[j - 1];
+		if (text[i] == text[j])
+			j += 1;
+		pi[i] = j;
 	}
-	while (globalCount++, array[stepLeft] < searchingNumber) {
-		stepLeft++;
-		if (stepLeft >= std::min(stepRight, len)) return 0;
+	return pi;
+}
+
+int kmpSearch(const char* text, const char* subText) {
+	int j = 0;
+	size_t lenText = strlen(text);
+	size_t lenSubText = strlen(subText);
+	int* pi = prefixGen(subText);
+	for (int i = 0; i < lenText; i++) {
+		for (; j > 0 && text[i] != subText[j];)
+			j = pi[j - 1];
+		if (text[i] == subText[j])
+			j++;
+		if (j >= lenSubText)
+			return i - j + 1;
 	}
-
-	if (globalCount++, array[stepLeft] == searchingNumber) return array[stepLeft];
-	return 0;
+	return -1;
 }
 
-int searchJumpLVL2(int* array, const int len, const int searchingNumber) {
-	const int jump = sqrt(len);
-	const int jump2 = sqrt(jump);
-	int stepLeft = 0;
-	int stepRight = jump;
-
-	while (globalCount++, array[std::min(stepRight, len) - 1] < searchingNumber) {
-		stepLeft = stepRight;
-		stepRight += jump;
-		if (stepLeft >= len) return 0;
+char* subStr(const char * text, int pointStart, const int pointEnd) {
+	int len = pointEnd - pointStart + 1;
+	char* subText = new char[len];
+	subText[len] = '\0';
+	for (int i = 0; i < len; i++) {
+		subText[i] = text[pointStart];
+		pointStart++;
 	}
+	return subText;
+}
 
-	int len2 = stepRight;
-	stepRight = stepLeft + jump2;
-
-	while (globalCount++, array[std::min(stepRight, len2) - 1] < searchingNumber) {
-		stepLeft = stepRight;
-		stepRight += jump2;
-		if (stepLeft >= len) return 0;
+bool equalsSubStr(char* text, const int len, int point1, int point2) {
+	if (point1 == point2) return true;
+	for (int i = 0; i < len; i++) {
+		if (text[point1] != text[point2]) return false;
 	}
-	while (globalCount++, array[stepLeft] < searchingNumber) {
-		stepLeft++;
-		if (stepLeft >= std::min(stepRight, len2)) return 0;
-	}
-
-	if (globalCount++, array[stepLeft] == searchingNumber) return array[stepLeft];
-	return 0;
+	return true;
 }
 
-int searchBinary(int* array, int begin, int end, const int searchingNumber) {
-	int mid;
-	while (begin <= end) {
-		mid = begin + (end - begin) / 2;
-		if (globalCount++, array[mid] == searchingNumber) return array[mid];
-		if (globalCount++, array[mid] > searchingNumber) end = mid - 1;
-		else begin = mid + 1;
-	}
-	return 0;
-}
+void checkSearchBySizeText(int functionSearch(const char*, const char*), int step) {
+	int maxLenText = step * 10;
+	int lenSubStr = 10;
+	for (int lenText = step; lenText <= maxLenText; lenText += step) {
+		int iters = 0;
+		char* text = genText(lenText);
+		double averageTime;
+		while (iters <= 100) {
+			int pointStartSubText = rand() % (lenText - lenSubStr -1);
+			char* subText = subStr(text, pointStartSubText, pointStartSubText + lenSubStr);
 
-int searchBinary(int* array, const int len, const int searchingNumber) {
-	return searchBinary(array, 0, len-1, searchingNumber);
-}
+			auto start = std::chrono::high_resolution_clock::now();
+			int result = functionSearch(text, subText);
+			auto end = std::chrono::high_resolution_clock::now();
 
-int searchInterpolation(int* array, int left, int right, const int searchingNumber) {
-	int index;
-	while (left <= right && (globalCount++, searchingNumber >= array[left]) && (globalCount++, searchingNumber <= array[right])) {
-		index = left + (((double)(right - left) / (array[right] - array[left])) * (searchingNumber - array[left]));
-		if (globalCount++, array[index] == searchingNumber) return array[index];
-		if (globalCount++, array[index] > searchingNumber) right = index - 1;
-		else left = index + 1;
-	}
-	return 0;
-}
-
-int searchInterpolation(int* array, const int len, const int searchingNumber) {
-	return searchInterpolation(array, 0, len - 1, searchingNumber);
-}
-
-int genUnevenNumber(int len) {
-	int number = rand() % (len-2);
-	if (number % 2 == 0) number++;
-	return number;
-}
-
-int genEvenNumber(int len) {
-	int number = rand() % (len-2);
-	if (number % 2 != 0) number++;
-	return number;
-}
-
-void checkSearchFunction(int funcSearch(int*, const int, const int), const int step) {
-	int maxLen = step * 10;
-	for (int nowLen = step; nowLen <= maxLen; nowLen += step) {
-		int searchingNumberExist;
-		int* array = genArray(nowLen);
-		int j = 0;
-		globalCount = 0;
-		std::cout << "LEN ARRAY: " << nowLen << std::endl;
-		while (j < 1000) {
-			searchingNumberExist = genUnevenNumber(nowLen);
-			if (funcSearch(array, nowLen, searchingNumberExist) == searchingNumberExist) j++;
+			if (equalsSubStr(text,lenSubStr,pointStartSubText,result)) iters++;
 			else {
-				std::cout << "FAIL" << std::endl;
+				std::cout << "------FAIL------" << std::endl;
+				std::cout << pointStartSubText << " :: " << result << std::endl;
+				std::cout << subText << " :: " << subStr(text, result, result + lenSubStr);
 				return;
 			}
+			averageTime += std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 		}
-		std::cout << "Existing - " << globalCount/j << std::endl;
-		globalCount = 0;
-		j = 0;
-		while (j < 1000) {
-			if (funcSearch(array, nowLen, genEvenNumber(nowLen))) {
-				std::cout << "FAIL" << std::endl;
-				return;
-			}
-			else j++;
-		}
-		std::cout << "NOT existing - " << globalCount/j << std::endl;
+		averageTime /= iters;
+		std::cout << "Size text: " << lenText << " T(n) = " << averageTime << std::endl;
 	}
 }
 
-int searchJumpForResearch(int *array, const int len, const int searchingNumber, const int jump) {
-	int stepLeft = 0;
-	int stepRight = jump;
 
-	while (globalCount++, array[std::min(stepRight,len)-1] < searchingNumber) {
-		stepLeft = stepRight;
-		stepRight += jump;
-		if (stepLeft >= len) return 0;
-	}
 
-	while (globalCount++, array[stepLeft] < searchingNumber) {
-		stepLeft++;
-		if (stepLeft >= std::min(stepRight,len)) return 0;
+std::ostream& operator << (std::ostream& os, const char* text) {
+	for (int i = 0; text[i] != '\0'; i++) {
+		std::cout << text[i];
 	}
-	
-	if (globalCount++, array[stepLeft] == searchingNumber) return array[stepLeft];
-	return 0;
-}
-
-void researchJump(const int step) {
-	int maxLen = step * 5;
-	int stepJump;
-	for (int nowLen = step; nowLen <= maxLen; nowLen += step) {
-		int* array = genArray(nowLen);
-		stepJump = 1;
-		std::cout << "LEN ARRAY:" << nowLen << "sqrt = "<< sqrt(nowLen) << std::endl;
-		while (stepJump <= nowLen / 2) {
-			int it;
-			globalCount = 0;
-			for (it = 1; it <= 100; it++) {
-				int searchingNumber = genUnevenNumber(nowLen);
-				if (searchJumpForResearch(array, nowLen, searchingNumber, stepJump) != searchingNumber) {
-					std::cout << "WTF " << searchingNumber << " WTF" << std::endl;
-					std::cout << "FAIL" << std::endl;
-					return;
-				}
-			}
-			std::cout << globalCount / it << std::endl;
-			stepJump++;
-		}
-	}
+	return os;
 }
 
 int main(void) {
-	srand(time(NULL));
-	int step = 10000000;
-	std::cout << "=======BINARY=======" << std::endl;
-	checkSearchFunction(searchBinary,step);
-	std::cout << "====================" << std::endl;
-	std::cout << "====INTERPOLATION====" << std::endl;
-	checkSearchFunction(searchInterpolation, step);
-	std::cout << "=====================" << std::endl;
-	std::cout << "======JUMPLVL1======" << std::endl;
-	checkSearchFunction(searchJumpLVL1, step);
-	std::cout << "====================" << std::endl;
-	std::cout << "======JUMPLVL2======" << std::endl;
-	checkSearchFunction(searchJumpLVL2, step);
-	std::cout << "====================" << std::endl;
-
-	/*for (int i = 0; i <= 100; i++) {
-		std::cout << genEvenNumber(step) << std::endl;
-	}*/
-	/*researchJump(step);*/
-
-	/*int* array = genArray(1000);
-	std::cout << array[5] << std::endl;
-	std::cout << searchBinary(array, 1000, 5) << std::endl;*/
-
+	srand((unsigned int)time(NULL));
+	const int step = 10000;
+	checkSearchBySizeText(kmpSearch, step);
 	return 0;
 }
