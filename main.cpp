61d0714907c2a43835727306d24f6502efd46035
Chekabell <nikita.toporischev@mail.ru>
2024-05-03 14:44:39 +0500

    fix

--- main.cpp
+++ main.cpp
@@ -24,15 +24,27 @@ pNode getMax(pNode node) {
 	return getMax(node->right);
 }
 
+pNode getMin(pNode node) {
+	if (!node) return nullptr;
+	if (!node->left) return node;
+	return getMin(node->left);
+}
+
 pNode insert(pNode node, Item key) {
-	pNode newNode = createNode(key);
+	pNode newNode = nullptr;
 	if (key >= node->key) {
-		if (!node->right) node->right = newNode;
+		if (!node->right) {
+			newNode = createNode(key);
+			node->right = newNode;
+		}
 		else insert(node->right, key);
 	}
 	else {
-		if (!node->left) node->left = newNode;
-		else insert(node->right, key);
+		if (!node->left) {
+			newNode = createNode(key);
+			node->left = newNode;
+		}
+		else insert(node->left, key);
 	}
 	return newNode;
 }
@@ -57,6 +69,49 @@ pNode remove(pNode node, Item key) {
 	}
 }
 
+pNode insertRoot(pNode node,Item key) {
+	pNode newNode = createNode(key);
+	if (!node) return newNode;
+	if (key > node->key) newNode->left = node;
+	else newNode->right = node;
+	return newNode;
+}
+
+void printTree(pNode node) {
+	if (!node) return;
+	printTree(node->left);
+	std::cout << node->key << std::endl;
+	printTree(node->right);
+}
+
+int height(pNode node){
+	if (!node) return 0;
+	return 1 + std::max(height(node->left), height(node->right));
+}
+
+int size(pNode node) {
+	if (!node) return 0;
+	return 1 + size(node->left) + size(node->right);
+}
+
 int main(void) {
+	srand(time(NULL));
+	pNode root = createNode(1);
+	int key = 0;
+
+	std::cout << "R : " << 1 << std::endl;
+	for (int i = 0; i < 5; i++) {
+		key = rand() % 10;
+		insert(root, key);
+		std::cout << i+1 << " : " << key << std::endl;
+	}
+	printTree(root);
+	std::cout << std::endl << "H : " << height(root) << std::endl;
+	std::cout << "S : " << size(root) << std::endl;
+	std::cout << remove(root, 1) << std::endl;
+	printTree(root);
+	std::cout << std::endl << "H : " << height(root) << std::endl;
+	std::cout << "S : " << size(root) << std::endl;
+
 	return 0;
 }
