0c034798037d16f492f24b8dea4441e9da7ab1ca
Chekabell <nikita.toporischev@mail.ru>
2024-05-16 14:20:05 +0500

    almost done

--- main.cpp
+++ main.cpp
@@ -1,160 +1,64 @@
 ﻿#include <iostream>
-#include <Windows.h>
+#include <vector>
 
-enum class Func { Ordered, OrderedBack, RandSeq, SawRand, SinRand, StairRand, QuaziRand, Last };
+#define M1 64
+#define M2 67
+#define K 200
 
-struct time {
-	int intTime;
-	int doubleTime;
-};
-	
-struct time a;
-
-//Функция для заполнения массива вещественными или целочисленными значениями в порядке возрастания
-template <typename T> void Ordered(T* arr, const int len, T step = 5) {
-	T tmp = 0;
-	for (int i = 0; i <= len; i++) {
-		arr[i] = tmp;
-		tmp += step;
+struct Date {
+	int month;
+	int year;
+	Date() {
+		month = 0;
+		year = 0;
+	}
+	Date(const int m, const int y) {
+		month = m;
+		year = y;
+	}
+	int genKey() {
+		return month * year;
+	}
+	Date& operator = (const Date& a){
+		month = a.month;
+		year = a.year;
+		return *this;
+	}
+	friend std::ostream& operator << (std::ostream &out, const Date* obj) {
+		std::cout << obj->month << "." << obj->year;
+		return out;
 	}
 };
 
-	//Функция для заполнения массива вещественными или целочисленными значениями в порядке убывания
-template <typename T> void OrderedBack(T* arr, const int len, T step = 5) {
-	T tmp = len;
-	for (int i = 0; i <= len; i++) {
-		arr[i] = tmp;
-		tmp -= step;
+class HashTableDate {
+private:
+	Date* array;
+	int sizeHashTable;
+public:
+	HashTableDate(const int size) {
+		sizeHashTable = size;
+		array = new Date[size];
 	}
-};
-
-//Функция для заполнения массива случайными вещественными или целочисленными значениями
-template <typename T> void RandSeq(T* arr, const int len, T max = 50) {
-	srand(time(0));
-	for (int i = 0; i < len; i++) {
-		arr[i] = (double)rand() / RAND_MAX * max;
+	int hashDate(const int key) {
+		return (int)(0.616161 * (float)key);
 	}
-};
-
-//Функция для заполнения массива целочисленными значениями в виде пилообразного распределения
-template <typename T> void SawRand(T* arr, const int len, int interval = 5, T min = 0, T max = 50) {
-	T tmp = min;
-	for (int i = 2; i <= len + 1; i++) {
-		arr[i - 2] = tmp;
-		tmp += (max - min) / interval;
-		if (tmp >= max || i % interval == 1) {
-			tmp = min;
-		}
+	int getIndex(const int key) {
+		return hashDate(key) % sizeHashTable;
 	}
-};
-
-//Функция для заполнения массива значениями в виде синусоидального распределения
-template <typename T> void SinRand(T* arr, const int len, T range = 50) {
-	for (int i = 0; i < len; i++) arr[i] = sin(i) * range;
-};
-
-//Функция для заполнения массива целочисленными значениями в виде ступенчатого распределения
-template <typename T> void StairRand(T* arr, const int len, T interval = 5, T mincoleb = 0, T maxcoleb = 50) {
-	srand(time(0));
-	T step = maxcoleb - mincoleb;
-	for (int i = 0, j = 0; i < len; i++, j++) {
-		if (j >= interval) {
-			j = 0;
-			mincoleb += step;
-			maxcoleb += step;
-		}
-		arr[i] = mincoleb + (double)rand() / RAND_MAX * (maxcoleb - mincoleb);
+	void add(int key, const struct Date* value){
+		array[getIndex(key)] = *value;
 	}
+	Date* operator [] (const int key) {
+		int index = getIndex(key);
+		return &array[index];
+	};
 };
-
-//Функция для заполнения массива целочисленными значениями в виде квазирандомного распределения
-template <typename T> void QuaziRand(T* arr, const int len, T min = 0, T max = 50, T step = 5) {
-	srand(time(0));
-	for (int i = 0; i < len; i++) {
-		arr[i] = min + (double)rand() / RAND_MAX * (max - min);
-		min += step;
-		max += step;
-	}
-};
-
-//Функция распределения для вызова функция по заполнению массивов данными
-void Decor(enum Func key, int* masInt, double* masDouble, const int len){
-	SYSTEMTIME st;
-	int tmpTimeInt = 0, tmpTimeDouble = 0;
-	GetLocalTime(&st);
-	tmpTimeInt = (st.wSecond * 1000) + st.wMilliseconds;
-	switch (key) {
-		case Func::Ordered:
-			Ordered<int>(masInt,len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			Ordered<double>(masDouble, len);
-			break;
-		case Func::OrderedBack:
-			OrderedBack<int>(masInt, len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			Ordered<double>(masDouble, len);
-			break;
-		case Func::RandSeq:
-			RandSeq<int>(masInt, len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			RandSeq<double>(masDouble, len);
-			break;
-		case Func::SawRand:
-			SawRand<int>(masInt, len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			SawRand<double>(masDouble, len);
-			break;
-		case Func::SinRand:
-			SinRand<int>(masInt, len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			SinRand<double>(masDouble, len);
-			break;
-		case Func::StairRand:
-			StairRand<int>(masInt, len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			StairRand<double>(masDouble, len);
-			break;
-		case Func::QuaziRand:
-			QuaziRand<int>(masInt, len);
-			GetLocalTime(&st);
-			tmpTimeDouble = (st.wSecond * 1000) + st.wMilliseconds;
-			QuaziRand<double>(masDouble, len);
-			break;
-	}
-	GetLocalTime(&st);
-	a.intTime = std::abs(tmpTimeDouble - tmpTimeInt);
-	a.doubleTime = std::abs((st.wSecond * 1000) + st.wMilliseconds - tmpTimeDouble);
-}
-
 
 int main(void) {
-	const int len = 100;
-	auto* massiveDouble = new double[len];
-	auto* massiveInt = new int[len];
-	Decor(Func::Ordered,massiveInt,massiveDouble,len);
-	std::cout << "Ordered Int = " << a.intTime << "\nOrdered Double = " << a.doubleTime << std::endl;
-	Decor(Func::OrderedBack, massiveInt, massiveDouble, len);
-	std::cout << "OrderedBack Int = " << a.intTime << "\nOrderedBack Double = " << a.doubleTime << std::endl;
-	Decor(Func::RandSeq, massiveInt, massiveDouble, len);
-	std::cout << "RandSeq Int = " << a.intTime << "\nRandSeq Double = " << a.doubleTime << std::endl;
-	Decor(Func::SawRand, massiveInt, massiveDouble, len);
-	std::cout << "SawRand Int = " << a.intTime << "\nSawRand Double = " << a.doubleTime << std::endl;
-	Decor(Func::SinRand, massiveInt, massiveDouble, len);
-	std::cout << "SinRand Int = " << a.intTime << "\nSinRand Double = " << a.doubleTime << std::endl;
-	Decor(Func::StairRand, massiveInt, massiveDouble, len);
-	std::cout << "StairRand Int = " << a.intTime << "\nStairRand Double = " << a.doubleTime << std::endl;
-	Decor(Func::QuaziRand, massiveInt, massiveDouble, len);
-	std::cout << "QuaziRand Int = " << a.intTime << "\nQuaziRand Double = " << a.doubleTime << std::endl;
-
-	/*QuaziRand<double>(massiveDouble, len);
-	for (int i = 0; i < len; i++) {
-		std::cout << massiveDouble[i] << (i%9!=0 || i==0 ? " ":"\n");
-	}*/
+	HashTableDate table = *new HashTableDate(M1);
+	Date* obj = new Date(5, 1975);
+	int key = obj->genKey();
+	table.add(key,obj);
+	std::cout << table[key] << std::endl;
 	return 0;
 }
